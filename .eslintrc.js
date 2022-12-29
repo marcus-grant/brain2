0fc6a7030eee923bf85d47b50ff2919682e76969
Marcus Grant <marcusfg@gmail.com>
2022-12-29 22:55:12 +0100

    Fix eslint problems
    * Download all needed packages for the eslint setup
    * The eslint setup was taken from an example starter repo
    * Fix some associated problems that arose from those packs
    * Fix some tsconfig stuff to make eslint work right

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,19 @@
 module.exports = {
   root: true,
+  extends: ['airbnb-typescript' /*"prettier"*/],
+  plugins: ['react', 'jsx-a11y', 'import'],
   parserOptions: {
     ecmaVersion: 2017,
+    project: ['tsconfig.json'],
   },
   overrides: [
     {
       files: ['*.ts', '*.tsx'],
       // processor: '@graphql-eslint/graphql',
-      parser: "@typescript-eslint/parser",
+      parser: '@typescript-eslint/parser',
       extends: [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended"
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
       ],
       env: {
         es6: true,
@@ -35,4 +38,46 @@ module.exports = {
       },
     },
   ],
-}
+};
+// {
+//   "extends": ["airbnb-typescript", "prettier"],
+//   "plugins": ["react", "jsx-a11y", "import"],
+//   "rules": {
+//     "no-unused-vars": "warn",
+//     "no-else-return": "off",
+//     "react/no-did-update-set-state": "off",
+//     "prefer-destructuring": "off",
+//     "react/prefer-stateless-function": "off",
+//     "react/prop-types": "off",
+//     "react/no-danger": "off",
+//     "react/button-has-type": "off",
+//     "react/jsx-filename-extension": "off",
+//     "react/no-array-index-key": "off",
+//     "react/destructuring-assignment": "off",
+//     "import/no-named-as-default": "off",
+//     "import/no-named-as-default-member": "off",
+//     "react/jsx-curly-brace-presence": "off",
+//     "react/jsx-one-expression-per-line": "off",
+//     "jsx-a11y/click-events-have-key-events": "off",
+//     "jsx-a11y/no-static-element-interactions": "off",
+//     "jsx-a11y/accessible-emoji": "off",
+//     "jsx-a11y/label-has-associated-control": "off",
+//     "jsx-a11y/label-has-for": "off",
+//     "eslint.autoFixOnSave": true,
+//     "jsx-a11y/anchor-is-valid": [
+//       0,
+//       {
+//         "components": ["Link"],
+//         "specialLink": ["hrefLeft", "hrefRight", "to"],
+//         "aspects": ["noHref", "invalidHref", "preferButton"]
+//       }
+//     ]
+//   },
+//   "settings": {
+//     "import/core-modules": []
+//   },
+//   "env": {
+//     "browser": true
+//   },
+//   "parser": "babel-eslint"
+// }
