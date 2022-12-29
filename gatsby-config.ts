aeb56f803a05fba708215f8b6af2e2298dbeb1b5
Marcus Grant <marcusfg@gmail.com>
2022-12-29 22:57:09 +0100

    Refactor some code based on new eslint config

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,22 +1,23 @@
-import type { GatsbyConfig } from "gatsby";
-
+// TODO: Convert to Typescript
+import type { GatsbyConfig } from 'gatsby';
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `brain2`,
-    siteUrl: `https://wiki.marcusgrant.dev`
+    title: 'Markipedia',
+    author: 'Marcus Grant',
+    siteUrl: 'https://wiki.marcusgrant.dev',
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
   plugins: [
-    "gatsby-plugin-image",
-    "gatsby-plugin-sitemap",
+    'gatsby-plugin-image',
+    'gatsby-plugin-sitemap',
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
-        "icon": "src/images/icon.png"
-      }
+        'icon': 'src/images/icon.png',
+      },
     },
     // TODO: No longer maintained, consider maintaining yourself
     // {
@@ -34,59 +35,60 @@ const config: GatsbyConfig = {
       },
     },
     // "gatsby-plugin-mdx",
-    "gatsby-plugin-postcss",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
+    'gatsby-plugin-postcss',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
     {
       resolve: 'gatsby-source-filesystem',
       options: {
-        "name": "images",
-        "path": "./src/images/"
+        'name': 'images',
+        'path': './src/images/',
       },
-      __key: "images"
+      __key: 'images',
     },
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/content/notes`,
-        name: `blog`,
+        name: 'notes',
+        // ignore: [`${__dirname}/content/notes/.zk`, `${__dirname}/content/notes/.zk`],
       },
     },
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `page-images`,
-        path: `${__dirname}/src/images`,
+        name: 'assets',
+        path: `${__dirname}/content/assets`,
       },
     },
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `images`,
+        name: 'images',
         path: `${__dirname}/content/notes`,
       },
     },
     {
-      resolve: `gatsby-transformer-remark`,
+      resolve: 'gatsby-transformer-remark',
       options: {
         plugins: [
           {
-            resolve: `gatsby-remark-images`,
+            resolve: 'gatsby-remark-images',
             options: {
               maxWidth: 800,
             },
           },
           {
-            resolve: `gatsby-remark-responsive-iframe`,
+            resolve: 'gatsby-remark-responsive-iframe',
             options: {
-              wrapperStyle: `margin-bottom: 1.0725rem`,
+              wrapperStyle: 'margin-bottom: 1.0725rem',
             },
           },
-          `gatsby-remark-prismjs`,
+          'gatsby-remark-prismjs',
         ],
       },
     },
-  ]
+  ],
 };
 
 export default config;
